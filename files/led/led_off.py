08a7355cd19f28532551aeeeb0477dd7186f3a68
G-code-SPOC <georg.auer@gmail.com>
2021-09-06 14:26:14 +0200

    on offf led mode

---
+++ files/led/led_off.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+
+# GPIO.output(23, GPIO.HIGH)
+# GPIO.output(24, GPIO.HIGH)
+# GPIO.output(25, GPIO.HIGH)
+
+GPIO.output(23, GPIO.LOW)
+GPIO.output(24, GPIO.LOW)
+GPIO.output(25, GPIO.LOW)
