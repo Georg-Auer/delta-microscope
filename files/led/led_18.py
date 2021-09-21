6eb208614e9b4ee6e498ea647474f8d6337b84ec
G-code-SPOC <georg.auer@gmail.com>
2021-09-21 13:19:29 +0200

    new led pin 18 (relais)

---
+++ files/led/led_18.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.OUT)
+except:
+    print("GPIOs already set or unavailable")
+
+# setting blue led to on
+GPIO.output(18, GPIO.HIGH)
+# time.sleep(5)
+# GPIO.output(23, GPIO.LOW)
+# GPIO.output(24, GPIO.HIGH)
+# time.sleep(5)
+# GPIO.output(24, GPIO.LOW)
+# GPIO.output(25, GPIO.HIGH)
+# time.sleep(5)
+# GPIO.output(25, GPIO.LOW)
+
+# GPIO.output(23, GPIO.LOW)
+# GPIO.output(24, GPIO.LOW)
+# GPIO.output(25, GPIO.LOW)
