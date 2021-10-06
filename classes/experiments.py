f7a1aaea99c99652bbf694a97a1848c9d7016164
Georg1986 <georg.auer@gmail.com>
2021-10-06 18:51:36 +0200

    datetime change

--- classes/experiments.py
+++ classes/experiments.py
@@ -94,11 +94,10 @@ class Experiment(object):
             if(self.experiment_running):
                 import time
                 with open(f"{self.exp_foldername}/environment.csv", "a") as log:
-                    # self.humidity, self.temperature = Adafruit_DHT.read_retry(DHT_SENSOR, self.dht_pin)
                     if self.humidity is not None and self.temperature is not None:                     
-                        log.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), self.temperature, self.humidity))
-                        from datetime import datetime as dt
-                        log.write(dt.now().strftime('%Y-%m-%d %H:%M:%S'), self.temperature, self.humidity)
+                        # log.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), self.temperature, self.humidity))
+                        log.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.temperature, self.humidity)
+                        # print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                         os.sync()
                         log.close()
                         return
