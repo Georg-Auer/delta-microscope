9be57109b78e5ca41367f5b4926a5f47e508095e
G-code-SPOC <georg.auer@gmail.com>
2021-10-15 11:04:07 +0200

    new arduino setup, needs more testing

--- raspberry-setup.sh
+++ raspberry-setup.sh
@@ -93,30 +93,37 @@ pip3 install torchvision -f https://torch.kmtea.eu/whl/stable.html
 # command line only install:
 # https://www.pjrc.com/teensy/td_download.html
 
-wget https://downloads.arduino.cc/arduino-1.8.16-linux64.tar.xz
-wget https://www.pjrc.com/teensy/td_154/TeensyduinoInstall.linux64
+mkdir Downloads
+cd ~/Downloads
+wget https://downloads.arduino.cc/arduino-1.8.16-linuxarm.tar.xz
+wget https://www.pjrc.com/teensy/td_155/TeensyduinoInstall.linuxarm
 wget https://www.pjrc.com/teensy/00-teensy.rules
 sudo cp 00-teensy.rules /etc/udev/rules.d/
-tar -xf arduino-1.8.16-linux64.tar.xz
-chmod 755 TeensyduinoInstall.linux64
-./TeensyduinoInstall.linux64 --dir=arduino-1.8.16
+tar -xf arduino-1.8.16-linuxarm.tar.xz
+cd arduino-1.8.16
+sudo ./install.sh
+cd ~/Downloads
+chmod 755 TeensyduinoInstall.linuxarm
+./TeensyduinoInstall.linuxarm --dir=arduino-1.8.16
 cd arduino-1.8.16/hardware/teensy/avr/cores/teensy4
 make
-wget https://www.pjrc.com/teensy/td_153/TeensyduinoInstall.linuxaarch64 # compatible with arduino 1.8.13
-sudo chmod 755 TeensyduinoInstall.linuxaarch64
-./TeensyduinoInstall.linuxaarch64
+cd ~/Downloads
+
+# wget https://www.pjrc.com/teensy/td_153/TeensyduinoInstall.linuxarm # compatible with arduino 1.8.13
+# sudo chmod 755 TeensyduinoInstall.linuxarm
+# ./TeensyduinoInstall.linuxarm
 #choose where you put the installation files in the GUI(!) with X11
-sudo rm -rf TeensyduinoInstall.linuxaarch64
-cd ~
-wget https://downloads.arduino.cc/arduino-1.8.16-linuxaarch64.tar.xz
-wget https://www.pjrc.com/teensy/td_154/TeensyduinoInstall.linuxaarch64
-wget https://www.pjrc.com/teensy/00-teensy.rules
-sudo cp 00-teensy.rules /etc/udev/rules.d/
-tar -xf arduino-1.8.16-linuxaarch64.tar.xz
-chmod 755 TeensyduinoInstall.linuxaarch64
-./TeensyduinoInstall.linuxaarch64 --dir=arduino-1.8.16
-cd arduino-1.8.16/hardware/teensy/avr/cores/teensy4
-make
+# sudo rm -rf TeensyduinoInstall.linuxarm
+# cd ~
+# wget https://downloads.arduino.cc/arduino-1.8.13-linuxarm.tar.xz
+# wget https://www.pjrc.com/teensy/td_154/TeensyduinoInstall.linuxarm
+# wget https://www.pjrc.com/teensy/00-teensy.rules
+# sudo cp 00-teensy.rules /etc/udev/rules.d/
+# tar -xf arduino-1.8.13-linuxarm.tar.xz
+# chmod 755 TeensyduinoInstall.linuxarm
+# ./TeensyduinoInstall.linuxarm --dir=arduino-1.8.13
+# cd arduino-1.8.13/hardware/teensy/avr/cores/teensy4
+# make
 
 
 # install for 64bit
