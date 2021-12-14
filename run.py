a515f3ae356729fe2583d5f929bed6e3d33d5814
Georg1986 <georg.auer@gmail.com>
2021-12-14 13:48:52 +0100

    debug load experiment

--- run.py
+++ run.py
@@ -598,6 +598,7 @@ def profile(experiment_name=None, action=None):
             current_experiment = select_flagged_experiment()
             logging.debug(f"Current experiment name(s): {current_experiment.name}")
             # return redirect(url_for('experiments'))
+            # return redirect(url_for('index'))
             # return experiment
     if action == 'zip_it':
         # current_experiment = select_flagged_experiment()
@@ -606,13 +607,15 @@ def profile(experiment_name=None, action=None):
         shutil.make_archive(f'app/{current_experiment.name}', 'zip', root_dir=IMAGEPATH, base_dir=current_experiment.name)
         # shutil.make_archive(f'{current_experiment.name}', 'zip', root_dir=IMAGEPATH, base_dir=current_experiment.name)
         return redirect(url_for('experiments'))
-    if action == 'save_it':
+    elif action == 'save_it':
         # current_experiment = select_flagged_experiment()
         logging.debug(f"Download zip of experiment: {current_experiment.name}")
         #For windows you need to use drive name [ex: F:/Example.pdf]
         print(f"{current_experiment.name}.zip")
         return send_file(f"{current_experiment.name}.zip", as_attachment=True)
         # return send_file(f"{current_experiment.name}.zip", as_attachment=True)
+    else:
+        return redirect(url_for('index'))
 
 def select_flagged_experiment():
     logging.debug(f"Current database lenght: {len(DATABASE)}")
